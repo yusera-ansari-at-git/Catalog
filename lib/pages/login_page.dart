81773b93b9e3f3ea08a4bcb5a563ed6ebe5e3244
yuserabano ansari <yuserabanoansari@yuserabanos-MacBook-Air.local>
2024-11-23 19:08:19 +0530

    added state

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,8 +2,17 @@ import 'package:catalog_app/pages/home_page.dart';
 import 'package:catalog_app/utils/app_routes.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget{
-  const LoginPage({super.key});
+class LoginPage extends StatefulWidget{
+const LoginPage({super.key});
+@override
+ State<LoginPage> createState()=>_LoginPageState();
+}
+class _LoginPageState extends State<LoginPage>{
+  
+ String name="";
+ bool buttonChanged=false;
+ TextEditingController nameController=TextEditingController(text: "");
+TextEditingController passwordController=TextEditingController(text: "");
   @override
   Widget build(BuildContext context){
     return Material(
@@ -11,7 +20,7 @@ class LoginPage extends StatelessWidget{
         child: Column(
           children: [
            Container(margin: EdgeInsets.all(20),
-           child:  Image.asset("assets/images/login_image.png", ),),
+           child:  Image.asset("assets/images/login_image.png", fit: BoxFit.cover,),),
            SizedBox(height: 20,),
            Text("Welcome", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
            SizedBox(height: 20,),
@@ -20,32 +29,78 @@ class LoginPage extends StatelessWidget{
           child: Column(
             children: [
                TextFormField(
+                controller:nameController ,
+                // onChanged: (value) {
+                //   setState(() {
+                //     name=value.toString();
+                //   });
+                // },
             decoration: InputDecoration(
               hintText: "enter user name",
-              label: Text("Name")
+              label: Text("Name $name")
             ),
            ),
            TextFormField(
+            controller: passwordController,
             obscureText: true,
-            decoration: InputDecoration(
+            decoration:const InputDecoration(
             
               hintText: "enter password",
               label: Text("Password")
             ),
            ),
-           SizedBox(height: 20,),
-           SizedBox(
-            width: MediaQuery.of(context).size.width *0.7,
-            child: ElevatedButton(onPressed:()=>{
-              // Navigator.of(context).push(MaterialPageRoute(builder: (context)=>
-              //    HomePage()
-              // )),
-              Navigator.of(context).pushNamed(AppRoutes.Home)
-           } ,
-           style: ElevatedButton.styleFrom(
-            // minimumSize: Size(200, 200),
-            backgroundColor: Colors.amber,
+           const SizedBox(height: 20,),
+           Material(
+
+            color: Colors.deepPurple,
+                  // shape: buttonChanged?BoxShape.circle:BoxShape.rectangle,
+             
+                  borderRadius:buttonChanged?BorderRadius.all(Radius.circular(50)): BorderRadius.all(Radius.circular(8)),
+
+   
+             child: InkWell(
+              // splashColor: Colors.red,
+              onTap: ()async {
+                setState(() {
+                  buttonChanged=true;
+                });
+                print(nameController.text);
+                await Future.delayed(Duration(seconds: 1));
+                // Navigator.pushNamed(context, AppRoutes.Home).then((res){
+                //   setState(() {
+                //     buttonChanged=false;
+                //   });
+
+                // });
+              },
+               child: AnimatedContainer(
+                duration: Duration(seconds: 1, ),
+                // padding: EdgeInsets.all(20),
+                width:buttonChanged?50: 150,
+                height: 50,
+                alignment: Alignment.center,
+                child:buttonChanged?Icon(Icons.done,color: Colors.blueAccent, ): Text("login", style: TextStyle(fontSize: 18),),
+                // decoration: BoxDecoration(
+                //   color: Colors.amber,
+                //   // shape: buttonChanged?BoxShape.circle:BoxShape.rectangle,
+             
+                //   borderRadius:buttonChanged?BorderRadius.all(Radius.circular(50)): BorderRadius.all(Radius.circular(8))
+                // ),
+               ),
+             ),
            )
+          //  SizedBox(
+          //   width: MediaQuery.of(context).size.width *0.7,
+          //   child: ElevatedButton(onPressed:()=>{
+          //     // Navigator.of(context).push(MaterialPageRoute(builder: (context)=>
+          //     //    HomePage()
+          //     // )),
+          //     Navigator.of(context).pushNamed(AppRoutes.Home)
+          //  } ,
+          //  style: ElevatedButton.styleFrom(
+          //   // minimumSize: Size(200, 200),
+          //   backgroundColor: Colors.amber,
+          //  )
           //  style: TextButton.styleFrom()
         //          style: const ButtonStyle(
         // elevation: WidgetStatePropertyAll(2),
@@ -54,8 +109,8 @@ class LoginPage extends StatelessWidget{
         
         // backgroundColor: WidgetStatePropertyAll(Colors.black)
         //          )
-           , child: Text("Submit"),),
-           ),
+          //  , child: Text("Submit"),),
+          //  ),
             ],
           ),)
           ],
